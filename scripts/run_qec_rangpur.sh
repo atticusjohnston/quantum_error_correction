05ee4edcfbbf08acd49e04e9b07a0c467f74c0ac
Atticus Johnston <atticusjohnston04@gmail.com>
2025-10-01 22:51:57 +0800

    .

--- scripts/run_qec_rangpur.sh
+++ scripts/run_qec_rangpur.sh
@@ -3,7 +3,7 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=4
 #SBATCH --gres=gpu:1
-#SBATCH --partition=test
+#SBATCH --partition=Test
 #SBATCH --mem=20G
 #SBATCH --time=01:00:00
 #SBATCH --job-name=qec_${CODE}_${TRICKY}
