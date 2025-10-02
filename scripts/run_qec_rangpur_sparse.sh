f79d98e004fee32743d8a0b84f6038f48c2122b3
Atticus Johnston <atticusjohnston04@gmail.com>
2025-10-02 15:42:02 +0800

    Sparse script

---
+++ scripts/run_qec_rangpur_sparse.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=4
+#SBATCH --gres=gpu:1
+#SBATCH --partition=a100-test
+#SBATCH --job-name=qec
+#SBATCH --output=logs/qec_%j.out
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=atticus.johnston@student.uq.edu.au
+
+source venv/bin/activate
+python main.py --code_type ${CODE} --tricky ${TRICKY} --log_level ${LOGGING} --sparse
